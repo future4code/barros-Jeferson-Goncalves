62a409cf793e20565fbb0adeae5a4290aa7a0470
jeferson do nascimento gonçalves <tsetegams@gmail.com>
2022-04-12 09:33:23 -0300

    Exercicios proposto e Desafio

---
+++ modulo1/aula_variaveis/desafio.js
@@ -0,0 +1,13 @@
+// ========== desafio
+
+let number1 = prompt("Digite um numero: ")
+let number2 = prompt("Digite segundo numero: ")
+
+number1 = Number(number1)
+number2 = Number(number2)
+
+let soma = number1 + number2
+let divisao = number1 / number2
+
+console.log(`A soma entre ${number1} e ${number2} é ${soma}`)
+console.log(`A divisao entre ${number1} e ${number2} é ${divisao}`)
