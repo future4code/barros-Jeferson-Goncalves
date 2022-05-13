8fe4c216aa0f01ec26fb2ded931dbbb1c00a27b3
Jeferson do nascimento goncalves <tsetegams@gmail.com>
2022-05-13 00:53:42 -0300

    Exercicios de revisao

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -19,8 +19,20 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  let arrayOrdenado = array.sort((a, b) => a-b)
-  return arrayOrdenado
+  console.log(array)
+  // let arrayOrdenado = array.sort((a, b) => a-b)
+  let arrayAux
+  for (let i = 0; i < array.length; i++){
+    let k = i + 1
+    for (let j = 0; j < array.length; j++){
+      if (array[j] > array[k]){
+        arrayAux = array[j]
+        array[j] = array[k]
+        array[k] = arrayAux
+      }
+    }
+  }
+  return array
 }
 
 // EXERCÍCIO 04
@@ -82,50 +94,102 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+  const numberPar = []
+  for (let i = 0; numberPar.length < n; i++){
+    if ((i % 2) === 0){
+      numberPar.push(i)
+    }
+  }
+  return numberPar
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+  let tipoTriangulo
+  if (ladoA != ladoB && ladoA != ladoC && ladoB != ladoC){
+    tipoTriangulo = "Escaleno"
+  } else if (ladoA === ladoB && ladoA != ladoC && ladoB != ladoC || ladoA === ladoC && ladoB != ladoC && ladoB != ladoA || ladoB === ladoC && ladoA != ladoC && ladoA != ladoB){
+    tipoTriangulo = "Isósceles"
+  }else if (ladoA === ladoB && ladoA === ladoC && ladoB === ladoC){
+    tipoTriangulo = "Equilátero"
+  }
+  return tipoTriangulo
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+  let arrayOrdenado = array.sort((a, b) => a-b)
+  const segundoMaior = arrayOrdenado[arrayOrdenado.length-2]
+  const segundoMenor = arrayOrdenado[1]
+  return [segundoMaior, segundoMenor]
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+  console.log(filme)
+  const imprimir = `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.toString().replaceAll(',', ', ')}.`
+  return imprimir
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+  pessoa.nome = "ANÔNIMO"
+  return pessoa
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+  const pessoasPermitida = []
+  for (dados of pessoas){
+    if (dados.idade < 60 && dados.idade > 14){
+      if (dados.altura > 1.5){
+        pessoasPermitida.push(dados)
+      }
+    }
+  }
+  return pessoasPermitida
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+  const pessoasNaoPermitida = []
+  for (dados of pessoas){
+    if(dados.idade > 60 || dados.idade <= 14 || dados.altura < 1.5){
+      pessoasNaoPermitida.push(dados)
+    }
+  }
+  return pessoasNaoPermitida
 }
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
-
+  for (conta of contas){
+    for(let i = 0; i < conta.compras.length; i++){
+      console.log(i)
+      conta.saldoTotal = conta.saldoTotal - conta.compras[i]
+    }
+    conta.compras = []
+  }
+  return contas
 }
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+  for(let i = 0; i < consultas.length; i++){
+    k = i+1
+    if (consultas[i].nome < consultas[k].nome){
+      consultas[i].nome = consultas[k].nome
+    }
+  }
+  return consultas
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
+  for (let i = 0; i < consultas[0].dataDaConsulta.length; i++){
+    consultas.sort((a, b) => { return a.dataDaConsulta.split("/")[i] - b.dataDaConsulta.split("/")[i]});
+  }
+  
+
+  return consultas
 }
